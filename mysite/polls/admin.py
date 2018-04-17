2293e7abe1ad1166dad3e58fc2cfa8eba4b7ff5a
MrVain <miklamf@gmail.com>
2018-04-17 16:23:31 +0800

    Add files via upload

---
+++ mysite/polls/admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Question
+from .models import Choice
+
+# Register your models here.
+
+
+
+
+admin.site.register(Question)
+admin.site.register(Choice)
